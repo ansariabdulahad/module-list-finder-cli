2a9f3e18beef8f516addac0be7e00e5401d3b5e7
Your Nameecho <you@example.com>
2024-05-16 19:35:49 +0530

    completed

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ import module
 
 def App() :
     print("-----List Finder CLI App-----")
-    name = input("Enter your name to find...\n")
+    name = input("Enter your name to find...\n").strip().lower()
     module.Finder(name)
 App()
--- module.py
+++ module.py
@@ -1,3 +1,21 @@
+index = 0
+count = 0
+
 def Finder(name) :
-    print(name)
-Finder()
+    global index
+    global count
+    list = ["ahad", "hamza", "noor", "samad", "ahAd", "ahad"]
+    data = list[index].lower()
+
+    if data == name :
+        count = count + 1
+
+    index = index + 1
+    length = len(list)
+    if index < length : 
+        Finder(name)
+    else :
+        if count != 0 :
+            print(f"{name} matched for {count} times")
+        else :
+            print("Index out of range")
